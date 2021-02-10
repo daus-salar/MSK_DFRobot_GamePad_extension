ea3bd128e72f3434be8a71e3c120da42fb4c1420
Mkurzava <Mkurzava@users.noreply.github.com>
2021-02-10 08:37:23 -0800

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,204 @@
-radio.setGroup(1)
-pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P15) == 0) {
-        radio.sendString("Open")
-    } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
-        radio.sendString("Close")
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
-        radio.sendString("LEDL")
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
-        radio.sendString("LEDR")
-    } else {
-        if (pins.analogReadPin(AnalogPin.P2) > 550 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
-            radio.sendString("F")
-        } else if (pins.analogReadPin(AnalogPin.P2) < 450 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
-            radio.sendString("B")
-        } else if (pins.analogReadPin(AnalogPin.P1) < 450 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
-            radio.sendString("L")
-        } else if (pins.analogReadPin(AnalogPin.P1) > 550 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
-            radio.sendString("R")
+namespace MSK_DFRobot_GamePad_NS
+{
+    /*
+    * Initialize the DFRobot GamePad driver extension.
+    * Include the Microbit Bluetooth Radio Group integer number
+    */
+    //% BlockID = msk_dfrobot_gamepad_initialize
+    //% block = "Initialize DFRobot GamePad"
+    export function initialize_GamePad (radioGroup: number)
+    {
+        radio.setGroup(radioGroup)
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
+        pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
+    }
+
+    /*
+    * Polls the status of the buttons A and B to see if either or both have been pressed
+    */
+    //% BlockID = msk_dfrobot_gamepad_pollAB
+    //% block = "Poll DFRobot GamePad buttons A and B"
+    export function poll_buttons_A_B (button_press_delay_msec: number)
+    {
+        if (input.buttonIsPressed(Button.AB)) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.setAudioPin(AnalogPin.P0)
+        } else if (input.buttonIsPressed(Button.A)) {
+            game_pad_output = "A"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                . # # . .
+                # . . # .
+                # # # # .
+                # . . # .
+                # . . # #
+                `)
+        } else if (input.buttonIsPressed(Button.B)) {
+            game_pad_output = "B"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # # # . #
+                `)
+            basic.pause(button_press_delay_msec)
         } else {
+            
+        }
+        basic.clearScreen()
+    }
+    /*
+    * Polls the status of the buttons joystick to see if the X or Y have been actuated or the Z button pressed
+    */
+    //% BlockID = msk_dfrobot_gamepad_pollJoyStick
+    //% block = "Poll DFRobot GamePad joystick"
+    export function poll_joystick (button_press_delay_msec: number)
+    {
+        if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+            game_pad_output = "X"
+            serial.writeLine(game_pad_output)
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.pause(button_press_delay_msec)
+        } else if (pins.analogReadPin(AnalogPin.P2) > 550 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+            game_pad_output = "FW" + convertToText(pins.analogReadPin(AnalogPin.P2))
+            serial.writeLine(game_pad_output)
+            radio.sendString(game_pad_output)
+            basic.pause(button_press_delay_msec)
+        } else if (pins.analogReadPin(AnalogPin.P2) < 450 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+            game_pad_output = "RV" + convertToText(pins.analogReadPin(AnalogPin.P2))
+            serial.writeLine(game_pad_output)
+            radio.sendString(game_pad_output)
+        } else if (pins.analogReadPin(AnalogPin.P1) > 550 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+            game_pad_output = "RT" + convertToText(pins.analogReadPin(AnalogPin.P1))
+            serial.writeLine(game_pad_output)
+            radio.sendString(game_pad_output)
+        } else if (pins.analogReadPin(AnalogPin.P1) < 450 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+            game_pad_output = "LT" + convertToText(pins.analogReadPin(AnalogPin.P1))
+            serial.writeLine(game_pad_output)
+            radio.sendString(game_pad_output)
+        } else {
+            basic.pause(20)
             radio.sendString("S")
         }
     }
-})
+    
+    /*
+    * Polls the status of the colored buttons 
+    */
+    //% BlockID = msk_dfrobot_gamepad_pollcoloredbuttons
+    //% block = "Poll DFRobot GamePad Color Buttons"
+    export function gamepad_poll_color_buttons (button_press_delay_msec: number) {
+        if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+            game_pad_output = "RED"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # . . # .
+                `)
+        } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
+            game_pad_output = "GREEN"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                . # # . .
+                # . . . .
+                # . # # .
+                # . . # .
+                . # # . .
+                `)
+            basic.pause(button_press_delay_msec)
+        } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
+            game_pad_output = "BLUE"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # # # . .
+                `)
+            basic.pause(button_press_delay_msec)
+        } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
+            game_pad_output = "YELLOW"
+            radio.sendString(game_pad_output)
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . . # . .
+                . . # . .
+                `)
+            basic.pause(button_press_delay_msec)
+        }
+        basic.clearScreen()
+    }
+    
+    /*
+    // Output string from GamePad functions
+    */
+    let game_pad_output = ""
+    
+    //initialize_GamePad(1)
+
+    /*
+    // Delay after actuating any GamePad buttons or the joysick
+    */
+    let button_delay_msec = 1000
+    //basic.forever(function ()
+    //{
+      //  gamepad_poll_color_buttons(button_delay_msec)
+      //  poll_buttons_A_B(button_delay_msec)
+      //  poll_joystick(button_delay_msec)
+    //}) 
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+MSK_DFRobot_GamePad_NS.initialize_GamePad(1)
+
+for (let i = 0; i<1000; i++)
+{
+    MSK_DFRobot_GamePad_NS.poll_buttons_A_B(200)
+
+    MSK_DFRobot_GamePad_NS.poll_joystick(200)
+
+    basic.pause(100)
+}
